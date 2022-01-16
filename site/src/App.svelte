99fce62e4447b3f23a8afc12637964dfad6a7f0c
Lucas Gelfond <8399069+lucasgelfond@users.noreply.github.com>
2022-01-15 16:07:42 -0800

    content

--- site/src/App.svelte
+++ site/src/App.svelte
@@ -1,30 +1,59 @@
 <script>
-	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>
+		hi, i'm lucas!
+		<br>
+		currently, i:
+	</p>
+		<ul>
+			<li>
+				lead reboot's <a href='https://reboothq.substack.com/p/fellowship-22'>student fellowship</a> 
+				(with my friend <a href='https://twitter.com/zhaovan8'>ivan</a>!), a cohort-based book club 
+				and writing workshop for 40 undergrads interested in themes of technology, humanity, and power 
+			</li>
+			<li>write about art, the internet, and technology&mdash;my writing has appeared in 
+				<a href='https://www.vice.com/en/contributor/lucas-gelfond'>vice</a>, 
+				<a href='https://logicmag.io/kids/gay-in-a-tiktok-way/'>logic</a>, 
+				<a href='https://reboothq.substack.com/p/cbpp'>reboot</a>, 
+				and <a href='http://theindy.org/search?query=lucas%20gelfond'>the college hill independent</a>. 
+				you can also find me on <a href='https://lucas.mirror.xyz/'>mirror</a> 
+				and <a href='https://lucasgelfond.substack.com/'>substack</a> </li>
+			<li>study computer science and english at brown; i'm currently finishing up my second year</li>
+		</ul>
+		<p>most recently, i've</p>
+		<ul>
+			<li>built tools for better civic participation as a software engineer in the nyc department 
+				of city planning's 
+				<a href='https://www1.nyc.gov/site/planning/planning-level/urban-design/urban-design-overview.page'>
+					urban design office</a> (through 
+					<a href='https://blog.codingitforward.com/meet-the-2021-corps-members-new-york-ny-e67adb4ac14f'>
+						coding it forward</a>)</li>
+			<li>taught in brown's <a href='http://ethics.cs.brown.edu/'>socially responsible computing</a> 
+				initiative, most recently for our largest intro class 
+				(<a href='https://cs.brown.edu/courses/cs015/'>CS15</a>), taught by professor andy van dam. 
+				you can see some of my lectures <a href=''>here</a>. </li>
+			<li>edited pieces for <a href='https://www.kernelmag.io/'>kernel magazine</a> and led 
+				editorial for the college hill independent's 
+				<a href='http://theindy.org/section=Science%20&%20Technology/0'>science and tech</a> section</li>
+		</ul>
+		<p>i'm most active on <a href='https://twitter.com/gucaslelfond'>twitter</a> and most reachable by 
+			<a href='mailto:lucas_gelfond@brown.edu'>email</a>&mdash;please say hi! 
+			
+			<br/> <br/>you can also find me on 
+			<a href='https://www.instagram.com/gucaslelfond/'>instagram</a>, 
+			<a href='https://www.goodreads.com/user/show/127129067-lucas-gelfond'>goodreads</a>,
+			 <a href="https://www.are.na/lucas-gelfond">are.na</a>, 
+			 <a href='https://letterboxd.com/lucasgelfond/'>letterboxd</a>, and 
+			 <a href='https://www.last.fm/user/lucasgelfond'>last.fm.</a> </p>
 </main>
 
 <style>
 	main {
-		text-align: center;
+		text-align: left;
 		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
