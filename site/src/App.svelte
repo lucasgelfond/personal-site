7c3f0f6f3a8a7891bd7b99fb4e38ca84309f08c0
Lucas Gelfond <8399069+lucasgelfond@users.noreply.github.com>
2022-01-29 22:49:26 -0500

    ok finally

--- site/src/App.svelte
+++ site/src/App.svelte
@@ -1,7 +1,7 @@
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@100&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono&display=swap" rel="stylesheet">
 </svelte:head>
 
