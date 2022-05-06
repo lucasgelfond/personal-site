5b48270c6603ac416fa4718e105537ef2c9857c4
Lucas Gelfond <lucas_gelfond@brown.edu>
2022-05-05 23:18:57 -0400

    i have to be SUCH an asshole on my website

--- site/src/App.svelte
+++ site/src/App.svelte
@@ -21,7 +21,7 @@
 		currently, i:
 	</p>
 		<ul>
-		<li>am based in providence, ri, finishing up my second year at brown, studying cs and english</li>
+		<li>am based in providence, ri, finishing up my second year at brown concentrating in computer science and english</li>
 		<li>work on <a href="https://www.zinemun.ch/p/yum?s=w">ZINE MUNCH</a>, an (irregular) newsletter/interview series about how people and publications shape public discourse and culture </li>
 			<li>write about the internet, art, and technology &mdash; my writing has appeared in 
 				<a href='https://www.vice.com/en/contributor/lucas-gelfond'>vice</a>, 
